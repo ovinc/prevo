872753c6c11a28038a566d51c4cf2c0a0aef5316
Olivier Vincent <ovinc.py@gmail.com>
2022-02-16 21:18:28 +0100

    Add GPLv3 license

--- prevo/record.py
+++ prevo/record.py
@@ -1,5 +1,24 @@
 """Record several sensors as a function of time with interactive CLI and graph."""
 
+# ----------------------------- License information --------------------------
+
+# This file is part of the prevo python package.
+# Copyright (C) 2022 Olivier Vincent
+
+# The prevo package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# The prevo package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with the prevo python package.
+# If not, see <https://www.gnu.org/licenses/>
+
 
 # Standard library imports
 from abc import ABC, abstractmethod
